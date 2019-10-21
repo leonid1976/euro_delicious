26b3e920000ffe5ed7c4a9790d16b80df6cc002c
Leonid Gaidarenko <leonid.neige@gmail.com>
2019-10-21 19:00:32 +1000

    Single page

--- app/js/my-js-Slider.js
+++ app/js/my-js-Slider.js
@@ -1,6 +1,24 @@
 (function() {
     'use strict';
-    var multiItemSlider = (function () {
+
+   var multiItemSlider = (function () {
+
+      function _isElementVisible(element) {
+        var rect = element.getBoundingClientRect(),
+          vWidth = window.innerWidth || doc.documentElement.clientWidth,
+          vHeight = window.innerHeight || doc.documentElement.clientHeight,
+          elemFromPoint = function (x, y) { return document.elementFromPoint(x, y) };
+        if (rect.right < 0 || rect.bottom < 0
+          || rect.left > vWidth || rect.top > vHeight)
+          return false;
+        return (
+          element.contains(elemFromPoint(rect.left, rect.top))
+          || element.contains(elemFromPoint(rect.right, rect.top))
+          || element.contains(elemFromPoint(rect.right, rect.bottom))
+          || element.contains(elemFromPoint(rect.left, rect.bottom))
+        );
+      }
+
       return function (selector, config) {
         var
           _mainElement = document.querySelector(selector), // основный элемент блока
@@ -14,15 +32,51 @@
           _positionLeftItem = 0, // позиция левого активного элемента
           _transform = 0, // значение транфсофрмации .slider_wrapper
           _step = _itemWidth / _wrapperWidth * 100, // величина шага (для трансформации)
-          _items = []; // массив элементов
+          _items = [], // массив элементов
+          _interval = 0,
+          _html = _mainElement.innerHTML,
+          _states = [
+            { active: false, minWidth: 0, count: 1 },
+            { active: false, minWidth: 980, count: 2 }
+          ],
+          _config = {
+            isCycling: false, // автоматическая смена слайдов
+            direction: 'right', // направление смены слайдов
+            interval: 2000, // интервал между автоматической сменой слайдов
+            pause: true // устанавливать ли паузу при поднесении курсора к слайдеру
+          };
 
+        for (var key in config) {
+          if (key in _config) {
+            _config[key] = config[key];
+          }
+        }
 
-        
-        
         // наполнение массива _items
         _sliderItems.forEach(function (item, index) {
           _items.push({ item: item, position: index, transform: 0 });
         });
+
+        var _setActive = function () {
+          var _index = 0;
+          var width = parseFloat(document.body.clientWidth);
+          _states.forEach(function (item, index, arr) {
+            _states[index].active = false;
+            if (width >= _states[index].minWidth)
+              _index = index;
+          });
+          _states[_index].active = true;
+        }
+
+        var _getActive = function () {
+          var _index;
+          _states.forEach(function (item, index, arr) {
+            if (_states[index].active) {
+              _index = index;
+            }
+          });
+          return _index;
+        }
 
         var position = {
           getItemMin: function () {
@@ -53,6 +107,9 @@
 
         var _transformItem = function (direction) {
           var nextItem;
+          if (!_isElementVisible(_mainElement)) {
+            return;
+          }
           if (direction === 'right') {
             _positionLeftItem++;
             if ((_positionLeftItem + _wrapperWidth / _itemWidth - 1) > position.getMax()) {
@@ -76,23 +133,99 @@
           _sliderWrapper.style.transform = 'translateX(' + _transform + '%)';
         }
 
+        var _cycle = function (direction) {
+          if (!_config.isCycling) {
+            return;
+          }
+          _interval = setInterval(function () {
+            _transformItem(direction);
+          }, _config.interval);
+        }
+
         // обработчик события click для кнопок "назад" и "вперед"
         var _controlClick = function (e) {
-          clearInterval(interval);
-          var direction = this.classList.contains('myslider__control_right') ? 'right' : 'left';
-          e.preventDefault();
-          _transformItem(direction);
+          if (this.classList.contains('myslider__control')) {
+            e.preventDefault();
+            var direction = this.classList.contains('myslider__control_right') ? 'right' : 'left';
+            _transformItem(direction);
+            clearInterval(_interval);
+            _cycle(_config.direction);
+          }
         };
 
+        // обработка события изменения видимости страницы
+        var _handleVisibilityChange = function () {
+          if (document.visibilityState === "hidden") {
+            clearInterval(_interval);
+          } else {
+            clearInterval(_interval);
+            _cycle(_config.direction);
+          }
+        }
+
+        var _refresh = function () {
+          clearInterval(_interval);
+          _mainElement.innerHTML = _html;
+          _sliderWrapper = _mainElement.querySelector('.myslider__wrapper');
+          _sliderItems = _mainElement.querySelectorAll('.myslider__item');
+          _sliderControls = _mainElement.querySelectorAll('.myslider__control');
+          _sliderControlLeft = _mainElement.querySelector('.myslider__control_left');
+          _sliderControlRight = _mainElement.querySelector('.myslider__control_right');
+          _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width);
+          _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width);
+          _positionLeftItem = 0;
+          _transform = 0;
+          _step = _itemWidth / _wrapperWidth * 100;
+          _items = [];
+          _sliderItems.forEach(function (item, index) {
+            _items.push({ item: item, position: index, transform: 0 });
+          });
+        }
+
         var _setUpListeners = function () {
-          // добавление к кнопкам "назад" и "вперед" обрботчика _controlClick для событя click
+          
           _sliderControls.forEach(function (item) {
             item.addEventListener('click', _controlClick);
+          });
+          
+          
+          _mainElement.addEventListener('click', _controlClick);
+          if (_config.pause && _config.isCycling) {
+            _mainElement.addEventListener('mouseenter', function () {
+              clearInterval(_interval);
+            });
+            _mainElement.addEventListener('mouseleave', function () {
+              clearInterval(_interval);
+              _cycle(_config.direction);
+            });
+          }
+          document.addEventListener('visibilitychange', _handleVisibilityChange, false);
+          window.addEventListener('resize', function () {
+            var
+              _index = 0,
+              width = parseFloat(document.body.clientWidth);
+  
+            
+            _states.forEach(function (item, index, arr) {
+
+              
+              if (width >= _states[index].minWidth)
+                _index = index;
+            });
+            
+            if (_index !== _getActive()) {
+              _setActive();
+            }
+             _refresh();
           });
         }
 
         // инициализация
         _setUpListeners();
+        if (document.visibilityState === "visible") {
+          _cycle(_config.direction);
+        }
+        _setActive();
 
         return {
           right: function () { // метод right
@@ -100,14 +233,46 @@
           },
           left: function () { // метод left
             _transformItem('left');
+          },
+          stop: function () { // метод stop
+            _config.isCycling = false;
+            clearInterval(_interval);
+          },
+          cycle: function () { // метод cycle 
+            _config.isCycling = true;
+            clearInterval(_interval);
+            _cycle();
           }
         }
 
       }
     }());
 
-    var slider = multiItemSlider('.myslider');
+    var slider = multiItemSlider('.myslider', {
+      isCycling: true
+    })
+
   
-    let interval = setInterval(slider.right, 2000);
+  
+  
+  
+  
+  
+  
+  
+  
+//
+//    var slider = multiItemSlider('.myslider');
+//  
+//    let interval = setInterval(slider.right, 2000);
+  
+//  window.addEventListener('resize', function(event){
+//    slider.stop();
+//    
+//    var slider = multiItemSlider('.myslider');
+//  
+//  
+//    let interval = setInterval(slider.right, 2000);
+//  });
     
     })();
